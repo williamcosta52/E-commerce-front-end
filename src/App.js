cf39742741bc6fd7413b306bb5e7ae51ccbeb7a0
Gabriela Oliveira <gabrielamontanhini@gmail.com>
2023-04-27 22:31:31 -0300

    Feat: visualização dos itens por categoria e do item em específico ao clicar

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import styled from "styled-components";
 import ExitPage from "./pages/ExitPage";
 import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
+import CategoryPage from "./pages/CategoryPage";
+import ItemPage from "./pages/ItemPage";
 
 export default function App() {
 	return (
 		<Container>
 			<BrowserRouter>
 				<Routes>
-					<Route path="/" element={<DarkPage />} />
+				{/*	<Route path="/" element={<DarkPage />} />
 					<Route path="/exit" element={<ExitPage />} />
 					<Route path="/home" element={<Home />} />
-					<Route path="/sign-up" element={<SignUp />} />
+	<Route path="/sign-up" element={<SignUp />} />*/}
+					<Route path="/:category" element={<CategoryPage />}/>
+					<Route path="/:category/:item" element={<ItemPage />}/>
 				</Routes>
 			</BrowserRouter>
 		</Container>
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+
+
+export default function Header(){
+    return (
+<HeaderPersonalizado>
+</HeaderPersonalizado>
+    )
+}
+
+const HeaderPersonalizado = styled.header`
+height: 120px;
+width: 100vw;
+background-color: #D9D9D9;
+`
